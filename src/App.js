67b092600635c2b8bab43914b4f4620bd526e0f0
alexangria1992 <alex040892@gmail.com>
2022-07-15 20:51:50 +1200

    React Menu Design

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <Menu />
       <Main>
         <Navbar />
-        <Wrapper>video cards</Wrapper>
+        <Wrapper>
+          <h1>test</h1>
+          <h1>test</h1>
+        </Wrapper>
       </Main>
     </Container>
   );
