5e8a220ff9148f025cf450df7f7d0c7403eb5c7e
alexangria1992 <alex040892@gmail.com>
2022-07-18 21:01:14 +1200

    Video Card Component

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+
+const Home = () => {
+  return (
+    <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: lightgreen;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export default Home;
