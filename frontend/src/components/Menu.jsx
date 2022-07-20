14d6b9fe0788fec3623a9837adc29f8c2cbbc0d8
alexangria1992 <alex040892@gmail.com>
2022-07-20 20:02:14 +1200

    React Router Dom Link

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -17,15 +17,18 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+import { Link } from "react-router-dom";
 
 const Menu = ({ darkMode, setDarkMode }) => {
   return (
     <Container>
       <Wrapper>
-        <Logo>
-          <Img src={AlexTube} />
-          AlexTube
-        </Logo>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Logo>
+            <Img src={AlexTube} />
+            AlexTube
+          </Logo>
+        </Link>
         <Item>
           <HomeIcon />
           Home
