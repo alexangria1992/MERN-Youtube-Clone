200dec2a021b3f6a3e0bc6d48cb830580c9efe8d
alexangria1992 <alex040892@gmail.com>
2022-07-26 21:40:01 +1200

    Create Rest API, Connect MongoDB Connection & create initial model, routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB".brightMagenta);
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(8800, () => {
-  console.log("Connected!as");
+  connect();
+  console.log("Connected to server!".brightYellow);
 });
