d7aace15035671f2cedfa2481d27ee52b7dd4530
alexangria1992 <alex040892@gmail.com>
2022-08-17 16:40:28 +1200

    Like and Dislike Functionality  + bug fix

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -21,7 +21,7 @@ const router = express.Router();
 router.post("/", verifyToken, addVideo);
 router.put("/:id", verifyToken, updateVideo);
 router.delete("/:id", verifyToken, deleteVideo);
-router.get("/find/:id", addVideo);
+router.get("/find/:id", getVideo);
 router.put("/view/:id", addView);
 router.get("/trend", trend);
 router.get("/random", random);
