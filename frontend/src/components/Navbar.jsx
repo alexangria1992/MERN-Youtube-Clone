ea0ecc6b7e3d40f5a0cc2921ad57f9df90d7a390
alexangria1992 <alex040892@gmail.com>
2022-08-20 19:52:43 +1200

    React Google Auth with MongoDB

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         {currentUser ? (
           <User>
             <VideoCallOutlinedIcon />
-            <Avatar />
+            <Avatar src={currentUser.img} />
             {currentUser.name}
           </User>
         ) : (
